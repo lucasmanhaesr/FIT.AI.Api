e4ab3694ee6b745697c105761148ac6c94533136
lucasmanhaesr <ramosmanhaes@outlook.com>
2026-03-04 15:20:21 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+    logger: true,
+});
+
+fastify.get("/", async function handler() {
+    return { hello: "world" };
+});
+
+try {
+    await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
